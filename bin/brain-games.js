aa85c67b4122a38f60868c8d232e124f749e6aa8
Daniil Shtarkman <95107643+Masterofowls@users.noreply.github.com>
2024-03-31 10:08:55 +0400

    Co-authored-by: Daniil Shtarkman <95107643+Masterofowls@users.noreply.github.com>

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 #!/usr/bin/env node
 
 import greetUser from '../src/cli.js';
 
+=======
+#!/usr/bin/env node
+
+import greetUser from '../src/cli.js';
+
+>>>>>>> 7d66de9 (Co-authored-by: Daniil Shtarkman <95107643+Masterofowls@users.noreply.github.com>)
 console.log('Welcome to the Brain Games!');
