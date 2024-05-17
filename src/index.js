2cce55a1d6ab13fc99b89393d120af70c6f1a320
Masterofowls <danvlad2015@gmail.com>
2024-05-17 03:58:32 -0700

    end3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 let name = '';
 export function gameStart(rules) {
-  console.log('Welcome to the Brain Games, friend!');
-  name = readlineSync.question('May I have your name, please ');
+  console.log('Welcome to the Brain Games!');
+  name = readlineSync.question('May I have your name');
   console.log(`Hello, ${name}!`);
   console.log(rules);
 }
