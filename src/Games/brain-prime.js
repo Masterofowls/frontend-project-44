c10482aa56d95d9f1ecd51f9598ec76236566bf9
Masterofowls <danvlad2015@gmail.com>
2024-05-18 05:12:48 -0700

    11

--- src/Games/brain-prime.js
+++ src/Games/brain-prime.js
@@ -1,6 +1,6 @@
 import { gameStart, generateGameInstance } from '../../src/index.js';
 
-gameStart('Type "yes" if given number is prime. Otherwise answer "no".');
+gameStart('Answer "yes" if given number is prime. Otherwise answer "no".');
 const primeNumbers = [
   2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
   73, 79, 83, 89, 97, 101, 103, 107,
