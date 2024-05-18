b6eccf79afb8cc35b9bbe3b057ff01cde5b4033a
Masterofowls <danvlad2015@gmail.com>
2024-05-18 05:06:22 -0700

    9

--- src/Games/brain-progression.js
+++ src/Games/brain-progression.js
@@ -11,7 +11,7 @@ import {
     const progressionNumbers = [Math.floor(Math.random() * 110)];
     const progressionIncrement = Math.floor(Math.random() * 10 + 1);
     for (let i = 0; i < 9; i += 1) {
-      progressionNumbers.push(progressionNumbers[j] + progressionIncrement);
+      progressionNumbers.push(progressionNumbers[i] + progressionIncrement);
     }
     const randomIndex = Math.floor(Math.random() * 10);
     missingNumber = progressionNumbers[randomIndex];
